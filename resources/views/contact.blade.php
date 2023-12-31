389e7f287801a0cbb0285445c9b6238173611714
3B032112 <rjoeyic@gmail.com>
2023-12-31 23:28:40 +0800

    edit heading

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,13 +8,12 @@
     <div class="container position-relative px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
-                <div class="site-heading">
-                    <h1>Clean Blog</h1>
-                    <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                <div class="page-heading">
+                    <h1>Contact Me</h1>
+                    <span class="subheading">Have questions? I have answers.</span>
                 </div>
             </div>
         </div>
-    </div>
 </header>
 <!-- Main Content-->
 <main class="mb-4">
